9a361dd78d2642290624cef7ead71c90f607b844
Thomas Arsouze <thomas.arsouze@zoho.com>
2021-04-21 10:57:36 +0200

    Pre-commit

--- is_number/tests/test_is_number.py
+++ is_number/tests/test_is_number.py
@@ -5,6 +5,7 @@ from is_number import is_number
 def test_is_number():
     assert is_number(1)
 
+
 def test_is_not_number():
     assert not is_number("Hello world")
     assert not is_number({"Hello": "world"})
